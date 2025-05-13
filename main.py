44e302e287ee258a571251af0ffcbf2c28d4faa3
Andy Liu <107045864+AndyLiu0330@users.noreply.github.com>
2025-05-12 18:03:56 -0700

    Refactor Player class initialization and add Rock class for falling objects

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #sprite
 import pygame
-
+import random
 # 初始化
 pygame.init()
 # 设置窗口大小
@@ -18,7 +18,8 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((50, 50))
         self.image.fill((0, 255, 0))  # 填充顏色
         self.rect = self.image.get_rect()
-        self.rect.center = (Width // 2, Height // 2)  # 設置初始位置
+        self.rect.x = (Width // 2) 
+        self.rect.bottom = Height-10 # 設置初始位置
         self.speedx = 8
     def update(self):
         key_pressed = pygame.key.get_pressed()
@@ -33,10 +34,35 @@ class Player(pygame.sprite.Sprite):
         if self.rect.x < 0:
             self.rect.x = 0
 
+class Rock(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((30, 40))
+        self.image.fill((255, 0, 0))  # 填充顏色
+        self.rect = self.image.get_rect()
+        self.rect.x = (random.randrange(0, Width-30))
+        self.rect.y = (random.randrange(-100, Height-40)) 
+        self.speedy = random.randrange(2, 10)
+        self.speedx = random.randrange(-3, 3)
+    def update(self):
+        key_pressed = pygame.key.get_pressed()
+        
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.top > Height or self.rect.left < 0 or self.rect.right > Width:
+            self.rect.x = (random.randrange(0, Width-30))
+            self.rect.y = (random.randrange(-100, Height-40)) 
+            self.speedy = random.randrange(2, 10)
+            self.speedx = random.randrange(-3, 3)
+
 all_sprites = pygame.sprite.Group()
 player = Player()
 all_sprites.add(player)
-    
+
+for i in range(8):
+    rock = Rock()
+    all_sprites.add(rock)
+
 
 running = True
 
